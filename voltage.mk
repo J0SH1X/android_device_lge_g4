80c9c221af12875a9e530b752a4c00f4ecd3fe49
J0SH1X <aljoshua.hell@gmail.com>
2025-06-21 22:36:08 +0000

    g4: cleanup, convert to voltage

--- voltage.mk
+++ voltage.mk
@@ -29,5 +29,5 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 
